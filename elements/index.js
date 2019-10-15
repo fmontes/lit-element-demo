9e416ccfa65e2236bfbd35d64a0dc1692e4df484
Freddy Montes <freddymontes@gmail.com>
2019-10-15 05:50:03 -0600

    Add rollup to pack

---
+++ elements/index.js
@@ -0,0 +1,3 @@
+import '../styles.css';
+import './library/app-button/app-button';
+import './library/app-textfield/app-textfield';
---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import embedCSS from 'rollup-plugin-embed-css';
+import minify from 'rollup-plugin-babel-minify';
+
+export default {
+    input: ['elements/index.js'],
+    output: {
+        dir: 'build',
+        format: 'esm',
+        sourcemap: true
+    },
+    plugins: [
+        embedCSS(),
+        resolve(),
+        babel(),
+        minify({
+            comments: false
+        })
+    ]
+};
