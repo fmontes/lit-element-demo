5f0094526e23f8a0055fd6866328d2726d504919
Freddy Montes <freddymontes@gmail.com>
2019-10-22 23:26:08 -0600

    Add demo for React

--- demos/react-wc/src/App.js
+++ demos/react-wc/src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import reactifyWc from 'reactify-wc';
+
+const Textfield = reactifyWc('app-textfield');
+const Button = reactifyWc('app-button');
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [title, setTitle] = useState();
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <div className="App-wcs">
+                    <h3>{title || 'Will show when you type'}</h3>
+                    <Textfield
+                        placeholder="Type something"
+                        onInput={({ originalTarget: { value } }) => setTitle(value)}
+                    />
+                    <Button
+                        data={{
+                            label: 'Show me',
+                            action: () => alert('Hello from React')
+                        }}
+                    />
+                </div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
