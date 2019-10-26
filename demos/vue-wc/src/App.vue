53d60af4d7f893ff5bf19c775a36604451999860
Freddy Montes <freddymontes@gmail.com>
2019-10-26 08:07:16 -0600

    Add demo for Vue

--- demos/vue-wc/src/App.vue
+++ demos/vue-wc/src/App.vue
@@ -1,28 +1,60 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <div class="wcs">
+      <h3>{{title || 'Will show when you type'}}</h3>
+      <app-textfield placeholder="Type something" @input="handleInput"></app-textfield>
+      <app-button :data="button" @click="handleClick"></app-button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import "lit-element-demo";
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  data: () => {
+    return {
+      title: "",
+      // TODO: need to find a way to pass object from Vue
+      button: JSON.stringify({
+        label: "Show me",
+        action: () => alert("Hello from Vuew")
+      })
+    };
+  },
+  methods: {
+    handleInput: function({ originalTarget: { value } }) {
+      this.title = value;
+    },
+    handleClick: function() {
+      alert("Click from Vue");
+    }
   }
-}
+};
 </script>
 
 <style>
+body,
+html {
+  height: 100%;
+}
+
+.wcs {
+  margin-top: 2rem;
+  text-align: center;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100%;
 }
 </style>
